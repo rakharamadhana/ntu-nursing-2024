eca36260febf914903748f8092b1e141920487c0
Ying-Kai Liao <yklworkingaddress@gmail.com>
2023-11-17 17:04:01 +0800

    get updated

---
+++ app/api/chatfuel/[...id]/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/lib/prismadb";
+
+interface Params {
+    id?: number;
+  }
+
+export async function GET(
+    request: Request,
+    { params }: { params: Params }
+) {
+  const { "id": messengerId } = params;
+
+  let chatfuel = await prisma.chatfuel.findUnique({
+    where: { messengerId },
+  });
+
+  if (chatfuel) {
+    return NextResponse.json(chatfuel);
+  }
+
+  return NextResponse.json('sorry not found');
+}
