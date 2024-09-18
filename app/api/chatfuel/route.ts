1a7d6461a6ad876a7c4d61828c0c10267f51b8ba
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2024-09-18 19:31:48 +0800

    Change the schema.prisma

--- app/api/chatfuel/route.ts
+++ app/api/chatfuel/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
-
 import prisma from "@/lib/prismadb";
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const { "messenger user id": messengerId } = body;
+  const { "messenger user id": messengerId, "student_id": studentId, "correct": score } = body;
 
   let chatfuel = await prisma.chatfuel.findUnique({
     where: { messengerId },
@@ -13,16 +12,18 @@ export async function POST(request: Request) {
   if (chatfuel) {
     chatfuel = await prisma.chatfuel.update({
       where: { messengerId },
-      data: { correct: { increment: 1 } },
+      data: { correct: score }, // Directly set the correct field to the new score
     });
 
     return NextResponse.json(chatfuel);
   }
 
+  // Create a new record if it doesn't exist
   chatfuel = await prisma.chatfuel.create({
     data: {
       messengerId,
-      correct: 1,
+      studentId,
+      correct: score, // Set the initial correct score from the request
     },
   });
 
